1b2ee7735498fa59bf517d34eaf826230263e09b
Bryce Monaco <brycedmonaco@gmail.com>
2019-08-07 22:34:02 -0700

    Player uses tweens to smoothly follow the path from its start to destination

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class PlayerController : MonoBehaviour 
 {
 	private PathFinding pathFinder;
 
 	public TileNode startNode;
 	public TileNode currentNode;
+
+    public float minMoveTime = 1f;  // A set of tweens from start position to end position will take at least this long (s)
+    public float maxMoveTime = 3f;  // A set of tweens from start position to end position will take no longer than this amount of time (s)
 
 	void Start () 
 	{
@@ -36,10 +39,14 @@ public class PlayerController : MonoBehaviour
 			{
 				TileNode hitNode = hit.collider.transform.parent.GetComponentInChildren <TileNode> ();
 
-				if (pathFinder.DoesAnyPathExist (currentNode, hitNode))
-				{
-					transform.position = hitNode.transform.position;
+                List<TileNode> path;
 
+				if ((path = pathFinder.DoesAnyPathExist (currentNode, hitNode)) != null)
+				{
+                    MoveAlongPath(path);
+
+                    Debug.Log("Path is " + path.Count + " tiles long.");
+                    //transform.position = hitNode.transform.position;
 					currentNode = hitNode;
 
 				} else 
@@ -60,4 +67,26 @@ public class PlayerController : MonoBehaviour
 		}
 		
 	}
+
+    void MoveAlongPath (List<TileNode> path)
+    {
+        float moveTime = minMoveTime;
+
+        if (path.Count > (int) maxMoveTime)
+        {
+            moveTime = maxMoveTime / (float) path.Count;
+        }
+
+        Vector3[] positions = new Vector3[path.Count];
+
+        for (int i = 0; i < path.Count; i++)
+        {
+            positions[i] = path[i].transform.GetComponentInChildren<TileNode>().transform.position;
+        }
+
+        transform.DOPath(positions, moveTime * path.Count);
+
+    }
+
+
 }
